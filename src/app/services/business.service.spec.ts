7c88d114e450e40653743f2f0a9f2eae158f3ce2
carlosgomescj7 <carlosgomes.cj7@gmail.com>
2025-09-29 22:01:08 -0500

    correcion de test

--- src/app/services/business.service.spec.ts
+++ src/app/services/business.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BusinessService } from './business.service';
 
@@ -6,7 +7,10 @@ describe('BusinessService', () => {
   let service: BusinessService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BusinessService]
+    });
     service = TestBed.inject(BusinessService);
   });
 
