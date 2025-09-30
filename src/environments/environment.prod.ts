a4d88e71fc85d7e582530b796a2c6b15aff575bb
carlosgomescj7 <carlosgomes.cj7@gmail.com>
2025-09-30 08:24:22 -0500

    se cambio la url para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Add your production environment variables here
-  apiUrl: 'http://localhost:3000', // Replace with your production API URL
+  apiUrl: 'http://ec2-18-191-148-249.us-east-2.compute.amazonaws.com:3000' // Replace with your production API URL
 };
